ccf69102eebe6c8398ef21e2e35231e9123a2c6e
daaasheng <daaasheng@gmail.com>
2020-06-24 18:03:03 +0800

    202.快乐数

---
+++ 202.快乐数.js
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=202 lang=javascript
+ *
+ * [202] 快乐数
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n) {
+    let sum = n
+    let nums;
+    let record = [];
+    while (!record.includes(sum) && sum !== 1 ) {
+        nums = [...String(sum)];
+        record.push(sum)
+        sum = 0;
+        for (let num of nums) {
+            sum += Number(num) ** 2
+        }
+        // console.log(sum)
+    }
+    return sum === 1
+};
+
+var testData = [
+    19,
+    7
+]
+
+for (let v of testData) {
+    console.log(isHappy(v))
+}
+// @lc code=end
+
