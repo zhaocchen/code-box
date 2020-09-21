c132c309855a3b49b9237d8090617b81103cefe9
zhaocchen <zhaocchen@163.com>
2020-09-21 23:49:05 +0800

    [226] 翻转二叉树 dfs

---
+++ 226.翻转二叉树.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=javascript
+ *
+ * [226] 翻转二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    if (root == null) return root;
+    [root.left, root.right] = [root.right, root.left];
+    invertTree(root.left);
+    invertTree(root.right);
+    return root;
+};
+// @lc code=end
+
