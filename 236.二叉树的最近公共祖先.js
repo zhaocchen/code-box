669f880e49a1bddc9494272d6a5d2ea3f120c2d1
zhaocchen <zhaocchen@163.com>
2020-09-21 22:46:27 +0800

    [236] 二叉树的最近公共祖先 LCA 问题

---
+++ 236.二叉树的最近公共祖先.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=236 lang=javascript
+ *
+ * [236] 二叉树的最近公共祖先
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    if (root == null) return null;
+    if (root.val == p.val || root.val == q.val) return root;
+    var left = lowestCommonAncestor(root.left, p, q);
+    var right = lowestCommonAncestor(root.right, p, q);
+    if (left != null && right != null) {
+        return root
+    } else if (left != null) {
+        return left
+    } else {
+        return right
+    }
+    return null
+};
+// @lc code=end
+
