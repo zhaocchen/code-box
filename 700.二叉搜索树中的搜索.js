ed331db80d70d9c7ad04286b1f90b6191e292193
zhaocchen <zhaocchen@163.com>
2020-11-07 21:44:19 +0800

    二叉搜索树中的搜索

---
+++ 700.二叉搜索树中的搜索.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=700 lang=javascript
+ *
+ * [700] 二叉搜索树中的搜索
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function(root, val) {
+    if (!root) return null;
+    if (root.val == val) return root;
+    else if (root.val > val) return searchBST(root.left, val);
+    else return searchBST(root.right, val);
+};
+// @lc code=end
