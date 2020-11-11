3606f1cbaf192619376a5406f7aa6a3a75711163
zhaocchen <zhaocchen@163.com>
2020-11-11 10:43:27 +0800

    fix

--- 202011/isMirror.js
+++ 202011/isMirror.js
@@ -1,11 +1,21 @@
+// function isMirror (val) {
+//     if (val < 10) return true;
+//     const endNum = val % 10;
+//     const count = (val + '').length - 1;
+//     const startNum = Math.floor(val / (10 ** count));
+//     // console.log(val, endNum, startNum)
+//     if (endNum != startNum) return false;
+//     return isMirror(Math.floor((val % (10 ** count)) / 10) )
+// }
+
 function isMirror (val) {
-    if (val < 10) return true;
-    const endNum = val % 10;
-    const count = (val + '').length - 1;
-    const startNum = Math.floor(val / (10 ** count));
-    // console.log(val, endNum, startNum)
-    if (endNum != startNum) return false;
-    return isMirror(Math.floor((val % (10 ** count)) / 10) )
+    let newVal = 0;
+    let oldVal = val;
+    while (oldVal) {
+        newVal = newVal * 10 + oldVal % 10;
+        oldVal = parseInt(oldVal / 10)
+    }
+    return newVal == val;
 }
 
 
