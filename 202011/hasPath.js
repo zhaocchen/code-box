1770e032bd2e14ccf0463c4d01e57fabb8bddd19
zhaocchen <zhaocchen@163.com>
2020-11-25 22:14:20 +0800

    clear repo

--- 202011/hasPath.js
+++
@@ -1,7 +0,0 @@
-
-
-function hasPath(root, val) {
-    if (root == null) return val == 0 ? 1 : 0;
-    const curVal = root.val;
-    return hasPath(root.left, val - curVal) || hasPath(root.right, val - curVal);
-}
